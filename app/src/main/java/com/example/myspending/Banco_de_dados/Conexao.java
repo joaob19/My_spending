0735fb0a36fb7b7d29c08d9b23a50d768bffd7b1
Mark1 <joao.b19@aluno.ifsc.edu.br>
2020-05-09 21:21:09 -0300

    - Sistema de gerenciamento de ativos pronto. - Relatório do mês pronto.

--- app/src/main/java/com/example/myspending/Banco_de_dados/Conexao.java
+++ app/src/main/java/com/example/myspending/Banco_de_dados/Conexao.java
@@ -3,19 +3,21 @@ package com.example.myspending.Banco_de_dados;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class Conexao extends SQLiteOpenHelper {
 public static final String nome="My spending database";
 public static  final int versao=1;
 
 public Conexao(Context context){
-super(context,"My spending database",null,1);
+super(context,nome,null,versao);
 }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table gastos(id integer primary key,nome varchar(50),valor float,mes integer,data varchar(20),categoria varchar(50))");
-        db.execSQL("create table ativos(id integer primary key, nome varchar(50),valor float)");
+        db.execSQL("create table if not exists gastos(id integer primary key,nome varchar(50),valor float,mes integer,data varchar(20),categoria varchar(50))");
+        db.execSQL("create table if not exists ativos(id integer primary key, nome varchar(50),valor float)");
+        Log.d("SQLite","OnCreate chamado");
     }
 
     @Override
